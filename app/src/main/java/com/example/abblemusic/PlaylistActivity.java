a91898ef0f01d2d2c33392bb893db259704318b0
Fouad Haddad <fouad.ha2003@gmail.com>
2020-10-27 19:58:54 +0200

    updated

--- app/src/main/java/com/example/abblemusic/PlaylistActivity.java
+++ app/src/main/java/com/example/abblemusic/PlaylistActivity.java
@@ -9,10 +9,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -187,5 +189,31 @@ public class PlaylistActivity extends AppCompatActivity implements AdapterView.O
             playing = song;
             player.start();
         }
+    }
+    public static void setListViewHeightBasedOnChildren
+            (ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) return;
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(listView.getWidth(),
+                View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            view = listAdapter.getView(i, view, listView);
+            if (i == 0) view.setLayoutParams(new
+                    ViewGroup.LayoutParams(desiredWidth,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+
+        params.height = totalHeight + (listView.getDividerHeight() *
+                (listAdapter.getCount()-1));
+
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
